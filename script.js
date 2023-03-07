3201678408c3847ede951844a15a4a11e740c0cc
ubadr <70177332+ubadr@users.noreply.github.com>
2023-03-07 17:01:14 +0400

    fixed struct - added scroll - abstract

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// let document.getElementById("title") = document.getElementById("document.getElementById("title")")
-
 document.addEventListener('click', () => {
-
     if(document.getElementById("title").innerText=="The"){
         document.getElementById("title").innerText="USB"
     }
@@ -13,3 +10,35 @@ document.addEventListener('click', () => {
         document.getElementById("main").style.display = 'flex'
     }
 });
+
+jQuery(document).ready(function ($) {
+    $(".open-contact-popup").click(function () {
+      $(".contact-popup").addClass("open");
+    });
+  
+    $(".close-contact-popup").click(function () {
+      $(".contact-popup").removeClass("open");
+    });
+  });
+  
+  gsap.registerPlugin(ScrollTrigger);
+  
+  function initScrollAnimations() {
+    gsap.to(".contact-popup", {
+      scrollTrigger: {
+        trigger: ".four",
+        start: "top center",
+        toggleClass: { targets: ".contact-popup", className: "open" },
+        scrub: true,
+        scroller: ".page-content"
+      }
+    });
+  }
+  
+  function init() {
+    initScrollAnimations();
+  }
+  
+  window.addEventListener("load", function () {
+    init();
+  });
