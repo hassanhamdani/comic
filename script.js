f90ba5156ff2f1edf723b8959176b20cb28d5dbb
FairyyGenie <ch3801@nyu.edu>
2023-03-09 11:43:34 +0400

    added comments

--- script.js
+++ script.js
@@ -1,72 +1,74 @@
-document.addEventListener('click', () => {
-    if(document.getElementById("title").innerText=="The"){
-        document.getElementById("title").innerText="USB"
-    }
-    else if (document.getElementById("title").innerText=="USB"){
-        document.getElementById("title").innerText="Curse"
-    }
-    else if(document.getElementById("title").innerText=="Curse"){
-        document.getElementById("mainContainer").style.display = 'none'
-        document.getElementById("main").style.display = 'flex'
-    }
+// function for landing page Titles
+document.addEventListener("click", () => {
+  if (document.getElementById("title").innerText == "The") {
+    document.getElementById("title").innerText = "USB";
+  } else if (document.getElementById("title").innerText == "USB") {
+    document.getElementById("title").innerText = "Curse";
+  } else if (document.getElementById("title").innerText == "Curse") {
+    document.getElementById("mainContainer").style.display = "none";
+    document.getElementById("main").style.display = "flex";
+  }
 });
 
+// function of sidebar
 jQuery(document).ready(function ($) {
-    $(".open-abstract-popup").click(function () {
-      $(".abstract-popup").addClass("open");
-    });
-  
-    $(".close-abstract-popup").click(function () {
-      $(".abstract-popup").removeClass("open");
-    });
-  });
-  
-  gsap.registerPlugin(ScrollTrigger);
-  
-  function initScrollAnimations() {
-    gsap.to(".abstract-popup", {
-      scrollTrigger: {
-        trigger: ".four",
-        start: "top center",
-        toggleClass: { targets: ".abstract-popup", className: "open" },
-        scrub: true,
-        scroller: ".page-content"
-      }
-    });
-  }
-  
-  function init() {
-    initScrollAnimations();
-  }
-  
-  window.addEventListener("load", function () {
-    init();
+  $(".open-abstract-popup").click(function () {
+    $(".abstract-popup").addClass("open");
   });
 
-var myVideo = document.getElementById('videoplay');
-var btn = document.getElementById('btn');
-btn.addEventListener('click', function () {
-if (myVideo.paused) {
-  if (myVideo.requestFullscreen) {
-      myVideo.requestFullscreen();
-  }
-  else if (myVideo.msRequestFullscreen) {
-      myVideo.msRequestFullscreen();
-  }
-  else if (myVideo.mozRequestFullScreen) {
-      myVideo.mozRequestFullScreen();
-  }
-  else if (myVideo.webkitRequestFullScreen) {
-      myVideo.webkitRequestFullScreen();
-  }
-  myVideo.play();
+  $(".close-abstract-popup").click(function () {
+    $(".abstract-popup").removeClass("open");
+  });
+});
+
+// scroll bar function
+gsap.registerPlugin(ScrollTrigger);
+
+function initScrollAnimations() {
+  gsap.to(".abstract-popup", {
+    scrollTrigger: {
+      trigger: ".four",
+      start: "top center",
+      toggleClass: { targets: ".abstract-popup", className: "open" },
+      scrub: true,
+      scroller: ".page-content",
+    },
+  });
 }
-else {
-  myVideo.pause();
+
+function init() {
+  initScrollAnimations();
 }
-}, false);
-myVideo.addEventListener('ended',myHandler,false);
+
+window.addEventListener("load", function () {
+  init();
+});
+
+// video function at the 7th frame
+var myVideo = document.getElementById("videoplay");
+var btn = document.getElementById("btn");
+btn.addEventListener(
+  "click",
+  function () {
+    if (myVideo.paused) {
+      if (myVideo.requestFullscreen) {
+        myVideo.requestFullscreen();
+      } else if (myVideo.msRequestFullscreen) {
+        myVideo.msRequestFullscreen();
+      } else if (myVideo.mozRequestFullScreen) {
+        myVideo.mozRequestFullScreen();
+      } else if (myVideo.webkitRequestFullScreen) {
+        myVideo.webkitRequestFullScreen();
+      }
+      myVideo.play();
+    } else {
+      myVideo.pause();
+    }
+  },
+  false
+);
+myVideo.addEventListener("ended", myHandler, false);
 function myHandler(e) {
-  myVideo.webkitExitFullScreen()
-  document.getElementById('seven').scrollIntoView();
+  myVideo.webkitExitFullScreen();
+  document.getElementById("seven").scrollIntoView();
 }
